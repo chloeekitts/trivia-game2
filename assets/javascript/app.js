4eb3983be2bdc8927fa643181403adc9970bb885
Chloe Kitts <47789998+chloeekitts@users.noreply.github.com>
2019-04-07 01:10:53 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,155 @@
+var triviaQuestions = [{
+	question: "Which university won the first NCAA Men's Basketball Championship?",
+	answerList: ["Ohio State", "Oregon", "Duke", "UCONN"],
+	answer: 1
+},{
+	question: "What unlikely member of the 1992 Olympic basketball team holds the record for most career points in the NCAA tournament?",
+	answerList: ["Larry Bird", "Michael Jordan", "Christian Laettner", "Magic Johnson"],
+	answer: 2	
+},{
+	question: "Who is the only #1 seed to lose to a #16 seed in the NCAA Men's Basketball tournament? (Hint: this happened in 2018)",
+	answerList: ["University of Virginia", "Duke", "UNC", "Kentucky"],
+	answer: 0
+},{		
+	question: "Which city has hosted the most NCAA Final Four games?",
+	answerList: ["New York City", "Washington, D.C.", "Atlanta", "Kansas City"],
+	answer: 3
+},{
+	question: "What NCAA team is the lowest seed to win the national title? (Hint: They were a #8 seed in 1985)",
+	answerList: ["Duke", "Villanova", "Michigan", "Syracuse"],
+	answer: 1
+},{
+	question: "This former Big East conference member was the last team to win a national title without having made the NCAA Tournament the previous season:",
+	answerList: ["Louisville", "Michigan", "Stanford", "UCONN"],
+	answer: 3
+},{
+	question: "This 1990’s national title winner holds the record for most points scored as a team in one NCAA tournament?",
+	answerList: ["UCLA", "Michigan State", "UNLV", "Oregon State"],
+	answer: 2
+},{
+	question: "In 2019, which team made it to the Sweet Sixteen for the first time since 1967?",
+	answerList: ["Gonzaga", "Auburn", "Virginia Tech", "Houston"],
+	answer: 2
+},{
+	
+}];
+
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
+}
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	$('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestion] +'.gif" width = "400px">');
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
